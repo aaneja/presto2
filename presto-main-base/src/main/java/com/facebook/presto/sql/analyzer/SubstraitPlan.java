80490d177d62e8df38f6dfcca5f9abbce7fe3808
Anant Aneja <1797669+aaneja@users.noreply.github.com>
2025-08-07 16:54:16 +0530

    Added basics for conversion
    Project, Filter, TableScan should work

--- presto-main-base/src/main/java/com/facebook/presto/sql/analyzer/SubstraitPlan.java
+++ presto-main-base/src/main/java/com/facebook/presto/sql/analyzer/SubstraitPlan.java
@@ -4,6 +4,7 @@ import com.facebook.presto.Session;
 import com.facebook.presto.common.type.DecimalType;
 import com.facebook.presto.common.type.StandardTypes;
 import com.facebook.presto.common.type.Type;
+import com.facebook.presto.metadata.FunctionAndTypeManager;
 import com.facebook.presto.metadata.Metadata;
 import com.facebook.presto.spi.ColumnHandle;
 import com.facebook.presto.spi.ConnectorTableHandle;
@@ -16,7 +17,9 @@ import com.facebook.presto.spi.relation.RowExpression;
 import com.facebook.presto.spi.relation.VariableReferenceExpression;
 import com.facebook.presto.sql.planner.Plan;
 import com.facebook.presto.sql.planner.plan.InternalPlanVisitor;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import io.substrait.dsl.SubstraitBuilder;
@@ -35,8 +38,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class SubstraitPlan
 {
@@ -73,14 +74,42 @@ public class SubstraitPlan
 
     private static io.substrait.plan.Plan.Root buildRoot(Plan plan, Metadata metadata, Session session)
     {
-        return null;
+        //TODO : Fix building the root
+        return plan.getRoot().accept(new Visitor(metadata, session), null);
     }
 
-    private class Visitor
+    private static class Visitor
             extends InternalPlanVisitor<Rel, Void>
     {
-        Metadata metadata;
-        Session session;
+        private final Metadata metadata;
+        private final Session session;
+        private final FunctionAndTypeManager functionAndTypeManager;
+        private final SubstraitRowExpressionVisitor substraitRowExpressionVisitor;
+
+        public Visitor(Metadata metadata, Session session)
+        {
+            this.metadata = metadata;
+            this.session = session;
+            functionAndTypeManager = metadata.getFunctionAndTypeManager();
+            substraitRowExpressionVisitor = new SubstraitRowExpressionVisitor(functionAndTypeManager.getFunctionAndTypeResolver());
+        }
+
+        /**
+         * Get a map of the output variables of a PlanNode to the FieldReferences
+         * There is an in-order 1:1 mapping between the output variables of a PlanNode and the sourceRel
+         *
+         * @param node
+         * @return
+         */
+        private Map<VariableReferenceExpression, FieldReference> getNodeOutputToFieldRefMap(PlanNode node, Rel nodeAsRel)
+        {
+            ImmutableMap.Builder<VariableReferenceExpression, FieldReference> builder = ImmutableMap.builder();
+            Preconditions.checkState(nodeAsRel.getRemap().isPresent(), "Remapping is not supported in Presto plans");
+            for (int i = 0; i < node.getOutputVariables().size(); i++) {
+                builder.put(node.getOutputVariables().get(i), b.fieldReference(nodeAsRel, i));
+            }
+            return builder.build();
+        }
 
         @Override
         public Rel visitPlan(PlanNode node, Void context)
@@ -88,45 +117,52 @@ public class SubstraitPlan
             return null;
         }
 
+        /**
+         * Project Node : I am not sure if I completely understood the semantics of remap in Substrait
+         * Presto's Project nodes can also drop source outputs, so possibly we need to handle that
+         * https://substrait.io/relations/basics/#emit-output-ordering
+         *
+         * @param node
+         * @param context
+         * @return
+         */
         @Override
         public Rel visitProject(ProjectNode node, Void context)
         {
             Rel sourceRel = node.getSource().accept(this, context);
-            List<FieldReference> sourceOutputs = getSourceOutputs(sourceRel);
-            // FIX ME, I need to add non-identity projections
-            // b.project(b->fieldReferences(sourceOutputs),)
-            //         // Convert the projections to Substrait expressions
-            //         node.getAssignments().getExpressions().stream()
-            //                 .map(x -> b.toSubstraitExpression(x, sourceOutputs))
-            //                 .collect(Collectors.toList()),
-            //         sourceRel);
-            // return super.visitProject(node, context);
+            Preconditions.checkState(sourceRel.getRemap().isPresent(), "Remapping is not supported in Presto plans");
 
-            return null;
-        }
+            ImmutableList.Builder<Expression> relAssignments = ImmutableList.builder();
+            Map<VariableReferenceExpression, FieldReference> sourceVariableRefs = getNodeOutputToFieldRefMap(node.getSource(), sourceRel);
+            for (Map.Entry<VariableReferenceExpression, RowExpression> entry : node.getAssignments().entrySet()) {
+                VariableReferenceExpression outVarRef = entry.getKey();
+                if (sourceVariableRefs.containsKey(outVarRef)) {
+                    // Identity projection, just add the source field reference as-is
+                    relAssignments.add(sourceVariableRefs.get(outVarRef));
+                    continue;
+                }
+                // Convert the RowExpression to a Substrait expression and add it to the assignments
+                relAssignments.add(toSubstraitExpression(entry.getValue(), sourceVariableRefs));
+            }
 
-        private List<FieldReference> getSourceOutputs(Rel sourceRel)
-        {
-            return IntStream.range(0, sourceRel.getRecordType().fields().size())
-                    .mapToObj(i -> FieldReference.newInputRelReference(i, sourceRel))
-                    .collect(Collectors.toList());
+            return b.project(
+                    input -> relAssignments.build(),
+                    b.remap(), // No remapping in Presto plans
+                    sourceRel);
         }
 
         @Override
         public Rel visitFilter(FilterNode node, Void context)
         {
             Rel sourceRel = node.getSource().accept(this, context);
-            // We need sourceRels output variables to build the predicate
             return b.filter(
-                    // Convert the predicate to a Substrait expression
-                    x -> toSubstraitExpression(node.getPredicate(), getSourceOutputs(sourceRel)),
+                    x -> toSubstraitExpression(node.getPredicate(), getNodeOutputToFieldRefMap(node.getSource(), sourceRel)),
                     sourceRel);
         }
 
-        private Expression toSubstraitExpression(RowExpression rowExpression,
-                List<FieldReference> fields)
+        private Expression toSubstraitExpression(RowExpression rowExpression, Map<VariableReferenceExpression, FieldReference> variableScopeMap)
         {
-            return null;
+            return rowExpression.accept(substraitRowExpressionVisitor, variableScopeMap);
         }
 
         @Override
@@ -152,6 +188,7 @@ public class SubstraitPlan
         /**
          * Convert Presto type to Substrait type
          * See https://substrait.io/types/type_system/
+         *
          * @param type
          * @return
          */
@@ -171,7 +208,7 @@ public class SubstraitPlan
                 case StandardTypes.DATE:
                     return R.DATE;
                 case StandardTypes.DECIMAL:
-                    return R.decimal(((DecimalType)type).getPrecision(), ((DecimalType)type).getScale());
+                    return R.decimal(((DecimalType) type).getPrecision(), ((DecimalType) type).getScale());
                 case StandardTypes.VARCHAR:
                     return R.STRING;
                 default:
---
+++ presto-main-base/src/main/java/com/facebook/presto/sql/analyzer/SubstraitRowExpressionVisitor.java
@@ -0,0 +1,160 @@
+package com.facebook.presto.sql.analyzer;
+
+import com.facebook.presto.common.QualifiedObjectName;
+import com.facebook.presto.common.function.OperatorType;
+import com.facebook.presto.common.type.BigintType;
+import com.facebook.presto.common.type.BooleanType;
+import com.facebook.presto.common.type.IntegerType;
+import com.facebook.presto.common.type.SmallintType;
+import com.facebook.presto.common.type.TinyintType;
+import com.facebook.presto.common.type.Type;
+import com.facebook.presto.spi.relation.CallExpression;
+import com.facebook.presto.spi.relation.ConstantExpression;
+import com.facebook.presto.spi.relation.InputReferenceExpression;
+import com.facebook.presto.spi.relation.LambdaDefinitionExpression;
+import com.facebook.presto.spi.relation.RowExpressionVisitor;
+import com.facebook.presto.spi.relation.SpecialFormExpression;
+import com.facebook.presto.spi.relation.VariableReferenceExpression;
+import com.facebook.presto.sql.relational.FunctionResolution;
+import io.substrait.dsl.SubstraitBuilder;
+import io.substrait.expression.Expression;
+import io.substrait.expression.FieldReference;
+import io.substrait.expression.FunctionArg;
+import io.substrait.extension.DefaultExtensionCatalog;
+import io.substrait.extension.SimpleExtension;
+import io.substrait.type.TypeCreator;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static java.lang.String.format;
+
+public class SubstraitRowExpressionVisitor
+        implements RowExpressionVisitor<Expression, Map<VariableReferenceExpression, FieldReference>>
+{
+    protected static final SimpleExtension.ExtensionCollection defaultExtensionCollection =
+            SimpleExtension.loadDefaults();
+    static final TypeCreator R = TypeCreator.of(false);
+    static final TypeCreator N = TypeCreator.of(true);
+    private final FunctionAndTypeResolver functionAndTypeResolver;
+    private final FunctionResolution functionResolution;
+    protected SubstraitBuilder b = new SubstraitBuilder(defaultExtensionCollection);
+
+    public SubstraitRowExpressionVisitor(FunctionAndTypeResolver functionAndTypeResolver)
+    {
+        this.functionAndTypeResolver = functionAndTypeResolver;
+        functionResolution = new FunctionResolution(functionAndTypeResolver);
+    }
+
+    public static Expression.ScalarFunctionInvocation and(Expression... args)
+    {
+        // If any arg is nullable, the output of or is potentially nullable
+        // For example: false or null = null
+        boolean isOutputNullable = Arrays.stream(args).anyMatch(a -> a.getType().nullable());
+        io.substrait.type.Type outputType = isOutputNullable ? N.BOOLEAN : R.BOOLEAN;
+        return scalarFn(DefaultExtensionCatalog.FUNCTIONS_BOOLEAN, "and:bool", outputType, args);
+    }
+
+    public static Expression.ScalarFunctionInvocation scalarFn(
+            String namespace, String key, io.substrait.type.Type outputType, FunctionArg... args)
+    {
+        SimpleExtension.ScalarFunctionVariant declaration =
+                defaultExtensionCollection.getScalarFunction(SimpleExtension.FunctionAnchor.of(namespace, key));
+        return Expression.ScalarFunctionInvocation.builder()
+                .declaration(declaration)
+                .outputType(outputType)
+                .arguments(Arrays.stream(args).collect(java.util.stream.Collectors.toList()))
+                .build();
+    }
+
+    @Override
+    public Expression visitVariableReference(VariableReferenceExpression reference, Map<VariableReferenceExpression, FieldReference> context)
+    {
+        return context.get(reference);
+    }
+
+    @Override
+    public Expression visitInputReference(InputReferenceExpression input, Map<VariableReferenceExpression, FieldReference> context)
+    {
+        throw new UnsupportedOperationException("Cant InputReferenceExpression to expression");
+    }
+
+    @Override
+    public Expression visitCall(CallExpression call, Map<VariableReferenceExpression, FieldReference> context)
+    {
+        if (functionResolution.isEqualsFunction(call.getFunctionHandle())) {
+            if (call.getArguments().size() != 2) {
+                throw new IllegalArgumentException("Equals function must have exactly two arguments: " + call);
+            }
+            Expression left = call.getArguments().get(0).accept(this, context);
+            Expression right = call.getArguments().get(1).accept(this, context);
+            return b.equal(left, right);
+        }
+        else if (functionResolution.isNotFunction(call.getFunctionHandle())) {
+            if (call.getArguments().size() != 1) {
+                throw new IllegalArgumentException("Not function must have exactly one argument: " + call);
+            }
+            Expression arg = call.getArguments().get(0).accept(this, context);
+            return scalarFn(DefaultExtensionCatalog.FUNCTIONS_BOOLEAN, "not:bool", R.BOOLEAN, arg);
+        }
+
+        QualifiedObjectName callName = functionAndTypeResolver.getFunctionMetadata(call.getFunctionHandle()).getName();
+        if (callName.equals(OperatorType.ADD.getFunctionName())) {
+            if (call.getArguments().size() != 2) {
+                throw new IllegalArgumentException("Equals function must have exactly two arguments: " + call);
+            }
+
+            Expression left = call.getArguments().get(0).accept(this, context);
+            Expression right = call.getArguments().get(1).accept(this, context);
+            return b.add(left, right);
+        }
+
+        if (callName.equals(OperatorType.SUBTRACT.getFunctionName())) {
+            if (call.getArguments().size() != 2) {
+                throw new IllegalArgumentException("Equals function must have exactly two arguments: " + call);
+            }
+
+            Expression left = call.getArguments().get(0).accept(this, context);
+            Expression right = call.getArguments().get(1).accept(this, context);
+            return b.subtract(left, right);
+        }
+
+        throw new UnsupportedOperationException("Cant CallExpression to expression");
+    }
+
+    @Override
+    public Expression visitConstant(ConstantExpression node, Map<VariableReferenceExpression, FieldReference> context)
+    {
+        Type type = node.getType();
+        if (type instanceof BooleanType) {
+            return b.bool((Boolean) node.getValue());
+        }
+        if (type instanceof BigintType || type instanceof TinyintType || type instanceof SmallintType
+                || type instanceof IntegerType) {
+            Number number = (Number) node.getValue();
+            return Expression.I64Literal.builder().value(number.longValue()).build();
+        }
+        throw new UnsupportedOperationException("Unsupported constant type: " + type.getDisplayName() +
+                " for constant: " + node.getValue() + " in expression: " + node);
+    }
+
+    @Override
+    public Expression visitLambda(LambdaDefinitionExpression lambda, Map<VariableReferenceExpression, FieldReference> context)
+    {
+        throw new UnsupportedOperationException("Cant LambdaDefinitionExpression to expression");
+    }
+
+    @Override
+    public Expression visitSpecialForm(SpecialFormExpression specialForm, Map<VariableReferenceExpression, FieldReference> context)
+    {
+        switch (specialForm.getForm()) {
+            case AND:
+                return and(specialForm.getArguments().stream().map(arg -> arg.accept(this, context)).toArray(Expression[]::new));
+            case OR:
+                return b.or(specialForm.getArguments().stream().map(arg -> arg.accept(this, context)).toArray(Expression[]::new));
+            default:
+                throw new UnsupportedOperationException(format("Unsupported special form: %s in expression: %s",
+                        specialForm.getForm(), specialForm));
+        }
+    }
+}
